515f0c4f7c910d3de4894dbb40c16394d25aebf5
lysolaka <lysolaka@gmail.com>
2026-05-15 14:50:22 +0200

    feat: implement serial port transactions

--- src/lib.rs
+++ src/lib.rs
@@ -7,8 +7,11 @@ mod gcode;
 mod svg;
 mod transport;
 
+use juk_cmd::cmd::Command;
+
 use cli::Args;
 use svg::Svg;
+use transport::Interface;
 
 /// Result type, equivalent to [`std::result::Result<T, Error>`].
 ///
@@ -23,6 +26,10 @@ pub enum Error {
     FmtError(#[from] std::fmt::Error),
     #[error("I/O error")]
     IoError(#[from] std::io::Error),
+    #[error("Data serialization / deserialization error")]
+    SerdeError(#[from] postcard::Error),
+    #[error("Serial communication error")]
+    SerialError(#[from] serialport::Error),
     #[error("SVG parsing error")]
     SvgError(#[from] roxmltree::Error),
     #[error("This error should not happen")]
@@ -46,11 +53,37 @@ macro_rules! bail {
 pub fn run(args: Args) -> crate::Result<()> {
     log::debug!("Running with: {:?}", args);
 
-
     let svg = bail!(Svg::open(&args.file), "Can't open the SVG file");
     let gcode = bail!(svg.emit_gcode(), "Failed to parse or emit the G-code");
 
     println!("{}", gcode);
+
+    log::info!("Opening the serial port, make sure the plotter is in binary mode");
+    let mut interface = bail!(
+        Interface::open(&args.port),
+        "Failed to connect to the plotter"
+    );
+
+    let commands = [
+        Command::ConfigGet {
+            key: "".to_string(),
+        },
+        Command::ConfigSet {
+            kv: vec![("k".to_string(), "v".to_string())],
+        },
+        Command::Cancel,
+        Command::Home {
+            x: true,
+            y: true,
+            z: false,
+        },
+    ];
+
+    for cmd in commands {
+        log::info!("Sent: {:?}", cmd);
+        let resp = interface.transaction(&cmd)?;
+        log::info!("Got : {:?}", resp);
+    }
 
     Err(Error::Infallible)
 }
--- src/transport.rs
+++ src/transport.rs
@@ -1,4 +1,61 @@
 //! Serial transport and [`Command`] - [`Response`] transactions.
 
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::time::Duration;
+
 use juk_cmd::cmd::Command;
 use juk_cmd::cmd::Response;
+
+use serialport::ClearBuffer;
+use serialport::DataBits;
+use serialport::Parity;
+use serialport::SerialPort;
+use serialport::StopBits;
+
+/// Wrapper around a serial port, providing a transaction interface for [`Command`] - [`Response`]
+/// communication.
+pub struct Interface {
+    serial: BufReader<Box<dyn SerialPort>>,
+}
+
+impl Interface {
+    /// Open the [`Interface`] at `port`.
+    pub fn open<S: AsRef<str>>(port: S) -> crate::Result<Self> {
+        log::debug!("Opening serial port at: {}", port.as_ref());
+        let serial = serialport::new(port.as_ref(), 115200)
+            .timeout(Duration::from_mins(5))
+            .data_bits(DataBits::Eight)
+            .stop_bits(StopBits::One)
+            .parity(Parity::None)
+            .open()?;
+
+        serial.clear(ClearBuffer::All)?;
+
+        if let Some(name) = serial.name() {
+            log::info!("Connected to: {}", name);
+        } else {
+            log::info!("Connected to: {}", port.as_ref());
+        }
+
+        Ok(Self {
+            serial: BufReader::with_capacity(32, serial),
+        })
+    }
+
+    /// Perform a transaction.
+    ///
+    /// # Warning
+    ///
+    /// This function may block indefinitely until an end-of-frame was received.
+    pub fn transaction(&mut self, cmd: &Command) -> crate::Result<Response> {
+        let output = postcard::to_stdvec_cobs(cmd)?;
+        self.serial.get_mut().write_all(&output)?;
+
+        let mut input = Vec::with_capacity(32);
+        let n = self.serial.read_until(0, &mut input)?;
+        let resp = postcard::from_bytes_cobs(&mut input[..n])?;
+
+        Ok(resp)
+    }
+}
